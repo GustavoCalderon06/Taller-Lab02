3f790aa42343e64d044c57516b02ec54701f2652
Gustavo <g.calderon05@ufromail.cl>
2022-09-21 10:45:43 -0300

    commit final

--- src/main/java/Sismos.java
+++ src/main/java/Sismos.java
@@ -1,11 +1,18 @@
+import java.util.Random;
 import java.util.Scanner;
+import java.util.function.Supplier;
 
 public class Sismos {
     public static void main(String[] args) {
         menu();
 
     }
+
     public static void menu() {
+        System.out.println("Ingrese dias");
+        double [][] matriz=generarMatriz(ingresar());
+        llenarMatrizAleatoria(matriz);
+
         int opcionIngresada;
 
         do{
@@ -13,7 +20,7 @@ public class Sismos {
             opcionIngresada = pedirOpcionMenu();
 
             switch (opcionIngresada) {
-                case 1 -> System.out.println("metodo1");
+                case 1 -> imprimirMatriz(matriz);
                 case 2 ->System.out.println("metodo2");
                 case 3 ->System.out.println("metodo3");
                 case 4 ->System.out.println("metodo4");
@@ -49,6 +56,32 @@ public class Sismos {
     }
     public static int ingresar(){
         return new Scanner(System.in).nextInt();
+    }
+    public static double[][] generarMatriz(int filas) {
+        return new double[filas][24];
+    }
+    public static double getNumeroRandom() {
+        return new Random().nextDouble(9.9);
+    }
+    public static void llenarMatrizAleatoria(double[][] matriz) {
+        llenarMatriz(matriz, Sismos::getNumeroRandom);
+    }
+    public static void llenarMatriz(double[][] matriz, Supplier<Double> generador) {
+        for (var fila = 0; fila < matriz.length; fila++) {
+            for (var columna = 0; columna < matriz[0].length; columna++) {
+                matriz[fila][columna] = generador.get();
+            }
+        }
+    }
+    public static void imprimirMatriz(double [][] matriz){
+        for (int x=0; x < matriz.length; x++) {
+            System.out.print("|");
+            for (int y=0; y < matriz[x].length; y++) {
+                System.out.print (matriz[x][y]);
+                if (y!=matriz[x].length-1) System.out.print("\t");
+            }
+            System.out.println("|");
+        }
     }
 
 
